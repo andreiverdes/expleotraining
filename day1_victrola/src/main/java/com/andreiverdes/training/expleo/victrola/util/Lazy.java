bf30d3fff9eb7a3b4f4afc153ea7b148d8b1cb25
Andrei <>
2020-02-25 02:45:36 +0200

    Make Lazy thread safe init

--- day1_victrola/src/main/java/com/andreiverdes/training/expleo/victrola/util/Lazy.java
+++ day1_victrola/src/main/java/com/andreiverdes/training/expleo/victrola/util/Lazy.java
@@ -1,46 +1,36 @@
 package com.andreiverdes.training.expleo.victrola.util;
 
-public class Lazy<T> {
+public class Lazy<Result> {
 
-    public static <T> Lazy<T> byLazy(Getter<T> getter) {
-        return new Lazy<>(getter);
+    public static <T> Lazy<T> byLazy(Initializer<T> initializer) {
+        return new Lazy<>(initializer);
     }
 
-    public static <T> Lazy<T> byLazy(Getter2<T> getter) {
-        return new Lazy<>(getter);
+    private Initializer<Result> initializer;
+
+    private volatile Result value;
+
+    private Lazy(Initializer<Result> initializer) {
+        this.initializer = initializer;
     }
 
-    private Getter<T> getter;
-    private Getter2<T> getter2;
-    private T value;
-
-    private Lazy(Getter<T> getter) {
-        this.getter = getter;
-    }
-
-    private Lazy(Getter2<T> getter) {
-        this.getter2 = getter;
-    }
-
-    public T get() {
-        if (value == null) {
-            value = getter.find();
+    public Result get() {
+        // use a temporary variable to reduce the number of reads of the
+        // volatile field
+        Result result = value;
+        if (result == null) {
+            synchronized (this) {
+                result = value;
+                if (result == null) {
+                    value = result = initializer.initialize();
+                }
+            }
         }
-        return value;
+        return result;
     }
 
-    public <K> T get(K param) {
-        if (value == null) {
-            value = getter2.find(param);
-        }
-        return value;
+    public interface Initializer<T> {
+        T initialize();
     }
 
-    public interface Getter<T> {
-        T find();
-    }
-
-    public interface Getter2<T> {
-        <K> T find(K param);
-    }
 }
