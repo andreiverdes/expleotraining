bc1171ead3549e4630626ba55cb271cc972798a6
Andrei <>
2020-02-25 12:36:56 +0200

    Step 6

--- day2_cinema/src/main/java/com/andreiverdes/training/expleo/cinema/ui/step4/SharedViewModel.java
+++ day2_cinema/src/main/java/com/andreiverdes/training/expleo/cinema/ui/step4/SharedViewModel.java
@@ -2,11 +2,23 @@ package com.andreiverdes.training.expleo.cinema.ui.step4;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.SavedStateHandle;
 import androidx.lifecycle.ViewModel;
 
 public class SharedViewModel extends ViewModel {
 
+    private static final String KEY_SEEKBAR_VALUE = "key_seekbar_value";
+
     private MutableLiveData<Integer> seekBarValue = new MutableLiveData<>();
+    private SavedStateHandle savedStateHandle;
+
+    public SharedViewModel(SavedStateHandle savedStateHandle) {
+        this.savedStateHandle = savedStateHandle;
+        Integer seekBarValue = this.savedStateHandle.get(KEY_SEEKBAR_VALUE);
+        if (seekBarValue != null) {
+            this.seekBarValue.setValue(seekBarValue);
+        }
+    }
 
     public LiveData<Integer> getSeekBarValue() {
         return seekBarValue;
@@ -14,5 +26,6 @@ public class SharedViewModel extends ViewModel {
 
     public void setSeekBarValue(Integer seekBarValue) {
         this.seekBarValue.postValue(seekBarValue);
+        savedStateHandle.set(KEY_SEEKBAR_VALUE, seekBarValue);
     }
 }
