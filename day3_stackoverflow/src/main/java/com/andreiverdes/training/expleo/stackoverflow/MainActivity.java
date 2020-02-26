8d4d39e2e08c65275d6d3142277401e04b06a3cc
Andrei <>
2020-02-26 09:01:49 +0200

    Add stackoverflow app

---
+++ day3_stackoverflow/src/main/java/com/andreiverdes/training/expleo/stackoverflow/MainActivity.java
@@ -0,0 +1,16 @@
+package com.andreiverdes.training.expleo.stackoverflow;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.andreiverdes.training.expleo.arch.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
